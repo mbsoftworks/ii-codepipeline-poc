712ae05393cb11e92645195bb2aec23b176b2736
Markus Bergkvist <markus.bergkvist@inter.ikea.com>
2023-03-16 11:58:15 +0100

    cdk init app

---
+++ infrstructure/test/infrstructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Infrstructure from '../lib/infrstructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infrstructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Infrstructure.InfrstructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
